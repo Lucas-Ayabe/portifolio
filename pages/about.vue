dd6c714b08fc587623725b649ac78d10b3d0887f
Lucas-Ayabe <lucas.ayabe@gmail.com>
2020-04-19 18:34:26 -0300

    add about page

---
+++ pages/about.vue
@@ -0,0 +1,44 @@
+<template>
+  <app-section class="container">
+    <h1 class="section__title">About</h1>
+    <br />
+    <br />
+    <br />
+    <br />
+    <br />
+    <br />
+    <br />
+    <br />
+    <br />
+    <br />
+    <br />
+    <br />
+    <br />
+    <br />
+    <br />
+    <br />
+    <br />
+    <br />
+    <br />
+    <br />
+    <br />
+    <br />
+    <br />
+    <br />
+    <br />
+    <br />
+    <br />
+    <br />
+    <br />
+  </app-section>
+</template>
+
+<script>
+import AppSection from '~/components/AppSection'
+export default {
+  name: 'Home',
+  components: {
+    AppSection
+  }
+}
+</script>
