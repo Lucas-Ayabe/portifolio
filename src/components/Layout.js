18beca826ce614e8f04b16ec939a576857a84f17
Lucas Akira Ayabe <lucas.ayabe@gmail.com>
2020-10-19 16:17:49 -0300

    detected user preference theme

--- src/components/Layout.js
+++ src/components/Layout.js
@@ -1,4 +1,4 @@
-import React, { useState } from "react"
+import React, { useState, useEffect } from "react"
 import HeaderBar from "./HeaderBar"
 import Container from "./Container"
 import Menu from "./Menu"
@@ -11,12 +11,20 @@ export default function Layout({ children }) {
   const [scrollY, setScrollY] = useState(0)
   const [isMenuOpen, setIsMenuOpen] = useState(false)
   const [isMenuFloating, setIsMenuFloating] = useState(false)
-  const [hasDarkTheme, setHasDarkTheme] = useState(true)
+  const [hasDarkTheme, setHasDarkTheme] = useState(false)
 
   const handleScroll = ({ target: { scrollTop } }) => {
     setScrollY(scrollTop)
     setIsMenuFloating(scrollY > 10)
   }
+
+  useEffect(() => {
+    return () => {
+      if (window.matchMedia("(prefers-color-scheme: dark)").matches) {
+        setHasDarkTheme(true)
+      }
+    }
+  }, [])
 
   return (
     <div
