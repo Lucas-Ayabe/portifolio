39337f0172fb7ae13e8c6252fdfb329c6ffd4e9d
Lucas Akira Ayabe <lucas.ayabe@gmail.com>
2020-10-19 16:23:02 -0300

    adjusted theme toggler responsive

--- src/components/ThemeToggler.js
+++ src/components/ThemeToggler.js
@@ -6,7 +6,7 @@ import { faSun, faMoon } from "@fortawesome/free-solid-svg-icons"
 const ThemeToggler = ({ onChange, state }) => {
   return (
     <button
-      class="theme-toggler [ button -fab ]"
+      className="theme-toggler [ button -fab ]"
       onClick={() => onChange(theme => !theme)}
     >
       {state && <FontAwesomeIcon size="2x" icon={faMoon} />}
